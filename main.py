59b8844b59dafda11eb55363c9f5748b2cb234a2
Paweł Modzelewski <modziasx@gmail.com>
2026-01-11 20:07:39 +0100

    adding clock to main.py

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
@@ -19,6 +21,8 @@ def main():
 
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000.0  # Limit to 60 FPS
+        # print(f"Delta time for this frame: {dt} seconds")
 
 
 if __name__ == "__main__":
